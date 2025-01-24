daa2068be418620104c965443f404dfaba9e2d29
221B125 <221b125@juetguna.in>
2025-01-24 09:47:49 +0530

    adding lab Exercise 1

---
+++ LE1/PROB 3/mother.java
@@ -0,0 +1,11 @@
+
+class Mother {
+    int x;
+
+    void show() {
+        System.out.println("Hello World");
+    }
+}
+
+
+
---
+++ LE1/PROB 4/Parameterized.java
@@ -0,0 +1,5 @@
+public class Parameterized {
+    public static void main(String args[]) {
+       
+}
+}
